f196a09a37c46c19e2f5c1bfafeb779e7b8fc5df
Ha Phan <phanducha310@gmail.com>
2021-09-01 09:01:41 +0700

    Install prettier lint-staged husky

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -8,33 +8,34 @@ const route = require('./routes');
 // HTTP logger
 // app.use(morgan('combined'));
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-    extended: true
-})); //handle data sent form HTMl
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+); //handle data sent form HTMl
 app.use(express.json()); //handle data sent from code js
 
 // XMLHttprequest,fetch,axios,       Can send data through get and post method
 
 // template engine
-app.engine('hbs', handlebars({
-    extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-//Home, search, contact 
-
+//Home, search, contact
 
 // routes init
 
 route(app);
 
-
-
 // 127.0.0.1 => localhost
 app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`))
+    console.log(`Example app listening at http://localhost:${port}`),
+);
