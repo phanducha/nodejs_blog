974cd5c63f217f62f96dd42686db6cb82d662cdd
Ha Phan <phanducha310@gmail.com>
2021-09-10 08:29:01 +0700

    set function select all and create method delete through action

--- nodejs/src/app/controllers/CourseController.js
+++ nodejs/src/app/controllers/CourseController.js
@@ -56,9 +56,23 @@ class CourseController {
         }
         //[PATCH] /course/:id/restore
     restore(req, res, next) {
-        Course.restore({ _id: req.params.id })
-            .then(() => res.redirect('back'))
-            .catch(next);
+            Course.restore({ _id: req.params.id })
+                .then(() => res.redirect('back'))
+                .catch(next);
+        }
+        //[PATCH] /course/handle-form-acion
+    handleFormActions(req, res, next) {
+        switch (req.body.action) {
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: 'Action is Invalid !!!' })
+
+        }
+
     }
 }
 
